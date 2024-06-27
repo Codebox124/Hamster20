1551117f983ad041375db292e4299116fdb36f03
Codebox124 <rajcodes733@gmail.com>
2024-06-27 18:18:29 +0100

    setting up

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import AboutUs from "./components/About";
+import Mission from "./components/Mission";
+import Roadmaps from "./components/Roadmaps";
+import Tokenomics from "./components/Tokenomics";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <AboutUs />
+      <Mission />
+      <Roadmaps />
+      <Tokenomics />
+      <Footer />
+    </div>
+  );
+}
