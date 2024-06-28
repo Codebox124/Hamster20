c46cc96a406d28ae45060704b6f3651096645bbd
Muh-Wale <muhammadolawale27@gmail.com>
2024-06-28 09:43:11 +0100

    done footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,62 @@
+import Image from 'next/image'
 import React from 'react'
+import { FaTelegramPlane } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='max-w-[1640px] text-white mx-auto'>
+      <div className='bg-[#532D00]'>
+        <div className=' max-w-[1400px] mx-auto py-10 px-3 md:px-0'>
+          <div className='flex justify-between item-center'>
+            <div className='flex gap-1 items-center'>
+              <div className='border border-white rounded-lg'>
+                <Image src='/Logo.png' width={50} height={50} alt=''/>
+              </div>
+              
+              <div className='uppercase font-extrabold'>
+                <h1 className=' text-xl'>Happy</h1>
+                <p className=' text-lg'>Hamster20</p>
+              </div>
+            </div>
+
+            <div className='uppercase md:flex gap-3 items-center hidden'>
+              <div><a href="">about us</a></div>
+              <div><a href="">mission</a></div>
+              <div><a href="">roadmap</a></div>
+              <div><a href="">tokenomics</a></div>
+            </div>
+
+            <div className='flex gap-5 items-center'>
+              <div className='p-3 rounded-lg bg-white text-[#532D00]'>
+                <a href=""><FaXTwitter size={22}/></a>
+              </div>
+
+              <div className='p-3 rounded-lg bg-white text-[#532D00]'>
+                <a href=""><FaTelegramPlane size={22}/></a>
+              </div>
+            </div>
+          </div>
+
+          <div className='uppercase flex justify-between items-center md:hidden mt-5 text-sm flex-wrap'>
+            <div><a href="">about us</a></div>
+            <div><a href="">mission</a></div>
+            <div><a href="">roadmap</a></div>
+            <div><a href="">tokenomics</a></div>
+          </div>
+
+          <div className='pt-8 text-center'>
+            <p className=' font-light text-sm leading-5 md:leading-9'>The information on the HappyHamster20 website is for informational purposes only and does not constitute financial investments, or legal advice. Cryptocurrency  investments involve a high degree of risk, including the potential loss of your entire investment. Always conduct your own research and consult witha a qualified financial advisor before making any investment decisions.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='bg-[#341C00]'>
+        <div className='max-w-[1400px] mx-auto py-3 px-3 md:px-0 flex flex-col md:flex-row justify-between items-center'>
+          <p>2024 by HappyHamster</p>
+          <p>All rights reserved</p>
+        </div>
+      </div>
+    </div>
   )
 }
--- app/components/Tokenomics.tsx
+++ app/components/Tokenomics.tsx
@@ -1,7 +1,91 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Tokenomics() {
   return (
-    <div>Tokenomics</div>
+    <>
+      <section className='bg-white max-w-[1640px] mx-auto'>
+        <div className='max-w-[1370px] mx-auto'>
+          <div className='flex justify-center'>
+            <button className=' bg-[#FFA500] p-2 rounded-md'>
+              Tokenomics
+            </button>
+          </div>
+
+          <div className='p-3 md:p-0'>
+            <div className=' my-8 md:my-16 grid grid-cols-2 gap-3 items-center md:items-start md:gap-8 w-fit'>
+              <div className=' col-span-1'>
+                <p className='  font-normal md:font-extrabold text-lg uppercase'>Circulation</p>
+                <p>200 Billion HPH</p>
+              </div>
+              <div className='flex gap-2 items-center col-span-1'>
+                <div className='p-7 bg-[#32CD32] rounded-md'></div>
+                <div>
+                  <p className=' font-normal md:font-extrabold text-base md:text-lg'>15%</p>
+                  <p className='font-thin text-sm md:text-base'>Airdrop Capital</p>
+                </div>
+              </div>
+              <div className='flex gap-2 items-center col-span-1'>
+                <div className='p-7 bg-[#1E90FF] rounded-md'></div>
+                <div>
+                  <p className=' font-normal md:font-extrabold text-base md:text-lg'>25%</p>
+                  <p className='font-thin text-sm md:text-base'>Presale Reserve</p>
+                </div>
+              </div>
+              <div className='flex gap-2 items-center col-span-1'>
+                <div className='p-7 bg-[#FF69B4] rounded-md'></div>
+                <div>
+                  <p className=' font-normal md:font-extrabold text-base md:text-lg'>30%</p>
+                  <p className='font-thin text-sm md:text-base'>Future Treasure</p>
+                </div>
+              </div>
+              <div className='flex gap-2 items-center col-span-1'>
+                <div className='p-7 bg-[#00CED1] rounded-md'></div>
+                <div>
+                  <p className=' font-normal md:font-extrabold text-base md:text-lg'>20%</p>
+                  <p className='font-thin text-sm md:text-base'>Marketing Reserve</p>
+                </div>
+              </div>
+              <div className='flex gap-2 items-center col-span-1'>
+                <div className='p-7 bg-[#FFA500] rounded-md'></div>
+                <div>
+                  <p className=' font-normal md:font-extrabold text-base md:text-lg'>10%</p>
+                  <p className='font-thin text-sm md:text-base'>Liquidity</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='bg-[#E1E1E1] max-w-[1640px] mx-auto my-16'>
+        <div className='max-w-[1360px] mx-auto flex items-center justify-between relative'>
+          <div className='flex flex-col justify-between h-full'>
+            <div className='mb-auto absolute -top-[70px] left-11'>
+              <Image src='/hamster image 3.png' width={130} height={130} alt='' />
+            </div>
+            <div className='mt-auto absolute -bottom-5 left-[220px]'>
+              <Image src='/hamster image 1.png' width={100} height={100} alt='' />
+            </div>
+          </div>
+
+          <div className='text-center max-w-[1200px] mx-4'>
+            <p className='text-[#F6A002] font-black text-7xl uppercase leading-[70px]'>
+              Spreading joy and financial freedom through innovative cryptocurrency solutions.
+            </p>
+          </div>
+
+          <div className='flex flex-col justify-between h-full'>
+            <div className='mb-auto  absolute right-20 top-[55px]'>
+              <Image src='/hamster image 4.png' width={80} height={80} alt='' />
+            </div>
+            <div className='mt-auto absolute -bottom-12 right-24'>
+              <Image src='/hamster image 2.png' width={150} height={150} alt='' />
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
   )
 }
