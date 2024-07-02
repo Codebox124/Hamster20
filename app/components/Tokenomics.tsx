955a2a5290902e32a80a4cb2623ab2be6c625412
Muh-Wale <muhammadolawale27@gmail.com>
2024-07-02 22:40:36 +0100

    added animation

--- app/components/Tokenomics.tsx
+++ app/components/Tokenomics.tsx
@@ -20,6 +20,21 @@ export default function Tokenomics() {
     },
   };
 
+  const containerVariant = {
+    hidden: { opacity: 0, y: 20, scale: 0.8 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      scale: 1,
+      transition: {
+        delay: 0.005,
+        duration: 0.25,
+        when: 'beforeChildren',
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
   const itemVariants = {
     hidden: { opacity: 0, y: 20, scale: 0.8 },
     visible: {
@@ -37,8 +52,9 @@ export default function Tokenomics() {
           <div className='flex justify-center tok_but'>
             <motion.button
               initial={{ y: -20, opacity: 0, scale: 0.8 }}
-              animate={{ y: 0, opacity: 1, scale: 1 }}
-              transition={{ duration: 0.8, ease: 'easeInOut' }}
+              whileInView={{ y: 0, opacity: 1, scale: 1 }}
+              viewport={{ once: false }}
+              transition={{ duration: 0.4, ease: 'easeInOut' }}
               className='bg-orange px-6 rounded-md py-2 text-white'
             >
               Tokenomics
@@ -48,14 +64,14 @@ export default function Tokenomics() {
           <motion.div
             className='p-3 xl:p-0 grid grid-cols-6 lg:col-span-7 items-center'
             initial='hidden'
-            animate='visible'
+            whileInView='visible'
+            viewport={{ once: false }}
             variants={containerVariants}
-            
           >
             <div className='lg:col-span-4 col-span-4 w-fit'>
               <motion.div
                 className='grid grid-rows-1 md:grid-cols-2 my-8 lg:my-16 gap-5 items-center md:items-start md:gap-14 w-fit'
-                variants={containerVariants}
+                variants={containerVariant}
               >
                 {[
                   { text: 'Circulation', value: '200 Billion HPH', class: 'tok_a' },
@@ -75,8 +91,9 @@ export default function Tokenomics() {
               <motion.div
                 className='bg-[#FFA500] flex justify-between items-center p-1 rounded-md w-[100%] tok_in'
                 initial={{ y: 50, opacity: 0, scale: 0.8 }}
-                animate={{ y: 0, opacity: 1, scale: 1 }}
-                transition={{ duration: 0.8, ease: 'easeInOut' }}
+                whileInView={{ y: 0, opacity: 1, scale: 1 }}
+                viewport={{ once: false }}
+                transition={{ duration: 0.4, ease: 'easeInOut' }}
               >
                 <div className='p-3 rounded-md bg-white text-[#666666]'>CA</div>
                 <div className='text-white uppercase overflow-hidden hidden md:block'>
@@ -94,8 +111,9 @@ export default function Tokenomics() {
             <motion.div
               className='z-40 col-span-2 object-cover md:block hidden tok_img'
               initial={{ x: 100, opacity: 0, scale: 0.8 }}
-              animate={{ x: 0, opacity: 1, scale: 1 }}
-              transition={{ duration: 0.8, ease: 'easeInOut' }}
+              whileInView={{ x: 0, opacity: 1, scale: 1 }}
+              viewport={{ once: false }}
+              transition={{ duration: 0.4, ease: 'easeInOut' }}
             >
               <div className='absolute inset-0 bg-gradient-to-r rounded-full blur-xl from-transparent via-[#f9a7037b] to-transparent opacity-50 gradient-overlay '></div>
               <img className='h-auto w-full object-cover' src='/tokenomics.png' alt='Hero Image' />
@@ -112,7 +130,8 @@ export default function Tokenomics() {
             <motion.div
               className='mb-auto absolute -top-[90px] xlg:-top-[70px] left-[10px] xlg:left-[70px] xl:left-11 aimg'
               initial={{ x: -100, opacity: 0, scale: 0.8 }}
-              animate={{ x: 0, opacity: 1, scale: 1 }}
+              whileInView={{ x: 0, opacity: 1, scale: 1 }}
+              viewport={{ once: false }}
               transition={{ duration: 0.8, ease: 'easeInOut' }}
             >
               <Image src='/hamster image 3.png' width={130} height={130} alt='' />
@@ -120,7 +139,8 @@ export default function Tokenomics() {
             <motion.div
               className='mt-auto absolute -bottom-5 lg:-bottom-7 left-[150px] lg:left-[170px] xlg:left-[220px] aimg'
               initial={{ x: -100, opacity: 0, scale: 0.8 }}
-              animate={{ x: 0, opacity: 1, scale: 1 }}
+              whileInView={{ x: 0, opacity: 1, scale: 1 }}
+              viewport={{ once: false }}
               transition={{ duration: 0.8, ease: 'easeInOut' }}
             >
               <Image src='/hamster image 1.png' width={100} height={100} alt='' />
@@ -130,7 +150,8 @@ export default function Tokenomics() {
             <motion.p
               className='text-yellow font-black text-2xl md:text-4xl lg:text-6xl xl:text-7xl uppercase leading-normal md:leading-[70px] text'
               initial={{ y: 50, opacity: 0, scale: 0.8 }}
-              animate={{ y: 0, opacity: 1, scale: 1 }}
+              whileInView={{ y: 0, opacity: 1, scale: 1 }}
+              viewport={{ once: false }}
               transition={{ duration: 0.8, ease: 'easeInOut' }}
             >
               {Array.from('Spreading joy and financial freedom through innovative cryptocurrency solutions.').map(
@@ -144,7 +165,8 @@ export default function Tokenomics() {
             <motion.div
               className='mb-auto absolute right-16 lg:right-4 xlg:right-28 xl:right-16 top-[55px] aimg'
               initial={{ x: 100, opacity: 0, scale: 0.8 }}
-              animate={{ x: 0, opacity: 1, scale: 1 }}
+              whileInView={{ x: 0, opacity: 1, scale: 1 }}
+              viewport={{ once: false }}
               transition={{ duration: 0.8, ease: 'easeInOut' }}
             >
               <Image src='/hamster image 4.png' width={80} height={80} alt='' />
@@ -152,7 +174,8 @@ export default function Tokenomics() {
             <motion.div
               className='mt-auto absolute -bottom-16 lg:-bottom-[75px] xlg:-bottom-12 right-20 xlg:right-24 aimg'
               initial={{ x: 100, opacity: 0, scale: 0.8 }}
-              animate={{ x: 0, opacity: 1, scale: 1 }}
+              whileInView={{ x: 0, opacity: 1, scale: 1 }}
+              viewport={{ once: false }}
               transition={{ duration: 0.8, ease: 'easeInOut' }}
             >
               <Image src='/hamster image 2.png' width={150} height={150} alt='' />
